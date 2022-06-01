ee27a0010c18c13b5c1df1a4135d4d9981ed80f1
AnchorI <yahorkakiselevus@gmail.com>
2022-06-01 16:48:15 +0300

    add edited check

--- src/bot.ts
+++ src/bot.ts
@@ -4,13 +4,14 @@ const bot = new Bot(""); // <-- put your authentication token between the ""
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
-bot.on("message:entities:url", (ctx) => {
+bot.on(["message:entities:url", "edited_message:caption_entities:url"], (ctx) => {
     const text = ctx.msg.text
 
-    if (text.includes("tiktok")) {
+    if (text?.includes("tiktok")) {
         ctx.deleteMessage()
         ctx.reply("Фу, боже")
     }
 })
+
 
 bot.start();
