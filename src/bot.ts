ec797ac509f912306959317fb936d62823236c6c
AnchorI <yahorkakiselevus@gmail.com>
2022-06-02 14:58:44 +0300

    another try

---
+++ src/bot.ts
@@ -0,0 +1,19 @@
+
+import { Bot } from "grammy";
+import { TOKEN } from "../config/config";
+
+const bot = new Bot(`"${TOKEN}"`); // <-- put your authentication token between the ""
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.on(["message:entities:url", "edited_message"], (ctx) => {
+    const text = ctx.msg.text
+
+    if (text?.includes("tiktok")) {
+        ctx.deleteMessage()
+        ctx.reply("Фу, боже")
+    }
+})
+
+
+bot.start();
