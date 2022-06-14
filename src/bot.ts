e99f36b5485f891507a2d1db17538ffdabd51490
AnchorI <yahorkakiselevus@gmail.com>
2022-06-14 17:19:12 +0300

    try new command

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,24 @@
-import { Bot } from "grammy";
-import config from "./config";
+import { Bot, Context } from "grammy";import config from "./config";
+    
+type MyContext = Context
 
-const bot = new Bot(`${config.token}`); // <-- put your authentication token between the ""
+const bot = new Bot<MyContext>(`${config.token}`); // <-- put your authentication token between the ""
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.command("MitchPidor", (ctx) => {
+    ctx.reply("Сам пидор")
+    ctx.replyWithSticker("https://www.gstatic.com/webp/gallery/1.webp")
+} )
+
+
+bot.hears("ping", async (ctx) => {
+    // `reply` is an alias for `sendMessage` in the same chat (see next section).
+    await ctx.reply("pong", {
+        // `reply_to_message_id` specifies the actual reply feature.
+        reply_to_message_id: ctx.msg.message_id,
+    });
+});
 
 bot.on(["message:entities:url", "edited_message"], (ctx) => {
     const text = ctx.msg.text
